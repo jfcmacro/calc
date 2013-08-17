edf2e6428a3ca477d28a477ac6803cc3c83c0527
Juan Francisco Cardona McCormick <jfcmacro@gmail.com>
2013-08-17 00:36:03 -0500

    An working evaluator. This is all for now

--- src/Language/Calc/Eval.hs
+++ src/Language/Calc/Eval.hs
@@ -1,4 +1,57 @@
 module Language.Calc.Eval where
 
+import Language.Calc.Token
 import Language.Calc.Expr
+import Language.Calc.Scanner
+import Language.Calc.Parser
+import Control.Monad
+import Control.Monad.State
+import System.IO(hPutStrLn, hPutStr, hFlush, hIsEOF, hGetLine, stdout, stderr, stdin)
+import System.Exit(exitSuccess)
+import Text.Parsec
 
+type EvalState = StateT Int IO
+
+evalExpr :: Expr -> EvalState Int
+evalExpr (EInt i) = return $ i
+evalExpr ERead  = get
+evalExpr (EStore expr) = do i <- evalExpr expr
+                            put i
+                            return $ i  
+evalExpr (EAdd lexpr rexpr) =  evalExpr2 (+) lexpr rexpr
+evalExpr (ESub lexpr rexpr) =  evalExpr2 (-) lexpr rexpr
+evalExpr (EMul lexpr rexpr) =  evalExpr2 (*) lexpr rexpr
+evalExpr (EDiv lexpr rexpr) =  evalExpr2 (div) lexpr rexpr
+
+evalExpr2 :: (Int -> Int -> Int) -> Expr -> Expr -> EvalState Int
+evalExpr2 op lexpr rexpr = do l <- evalExpr lexpr
+                              r <- evalExpr rexpr
+                              return $ (l `op` r) 
+
+callEval :: Expr -> EvalState ()
+callEval expr = do
+  put 0
+  r <- evalExpr expr
+  lift $ hPutStrLn stdout $ show r
+  
+errParser :: ParseError -> EvalState ()
+errParser e = lift $ hPutStrLn stderr $ show e
+
+
+parseExpr :: Tokens -> EvalState ()
+parseExpr tkns = either errParser callEval (parse pExpr "Std input" tkns)
+
+
+process :: EvalState ()
+process = do
+  lift $ hPutStr stdout "> "
+  lift $ hFlush stdout
+  l <- lift $ hGetLine stdin
+  isEOF <- lift $ hIsEOF stdin
+  if isEOF || null l
+   then lift $ exitSuccess
+   else do either errParser parseExpr ( parse pTokens "Std input" l)
+           process
+
+runEval :: IO ()
+runEval = evalStateT process 0
--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,58 @@ import Language.Calc.Parser
 import Language.Calc.Scanner
 import Language.Calc.Token
 import Language.Calc.Expr
+import Language.Calc.Eval
+import System.Environment(getArgs)
+import Data.Version(Version(..), showVersion)
+import System.Console.GetOpt
+import System.IO(hPutStrLn, stdout, stderr)
+import Paths_calc(version)
+
+data Options =  Options { optShowVersion   :: Bool
+                        , optShowOutScan   :: Bool
+                        , optShowOutParser :: Bool
+                        , optShowHelp      :: Bool
+                        } deriving Show
+
+defaultOptions :: Options
+defaultOptions =  Options { optShowVersion   = False
+                          , optShowOutScan   = False
+                          , optShowOutParser = False
+                          , optShowHelp      = False
+                          }
+
+options :: [OptDescr (Options -> Options)]
+options =
+  [ Option ['v'] ["version"] 
+    (NoArg (\opts -> opts { optShowVersion  = True })) 
+    "show version number"
+  , Option ['s'] ["scanner"] 
+    (NoArg (\opts -> opts { optShowOutScan = True }))
+    "show scanner output"
+  , Option ['p'] ["parser"] 
+    (NoArg (\opts -> opts { optShowOutParser = True }))
+    "show parser output"
+  , Option ['h','?'] ["help"] 
+    (NoArg (\opts -> opts { optShowHelp = True }))
+    "show this output"
+  ]
+
+helpHeader :: String
+helpHeader = "Usage: calc [OPTION..]"
+
+compilerOpts :: [String] -> IO (Options, [String])
+compilerOpts argv =
+  case getOpt Permute options argv of
+    (o,n,[]) -> return (foldl (flip id) defaultOptions o,n)
+    (_,_,errs) -> ioError (userError (concat errs ++ usageInfo header options))
+    where header = helpHeader
+
 
 main :: IO ()
-main = undefined
+main = do
+  (opts, fls) <- (getArgs >>= compilerOpts)
+  if (optShowHelp opts)
+   then hPutStrLn stdout $ usageInfo helpHeader options
+   else runEval
+
+  
